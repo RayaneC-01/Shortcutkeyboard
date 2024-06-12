e1dfd80991e9aef0e1cb2f14ace3862c1a0ada64
Rayane <rchaabane20@gmail.com>
2024-06-12 23:23:05 +0200

    Page de contact

--- PHP/contact.php
+++ PHP/contact.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Contact - Présentation des Véhicules</title>
+    <title>Page de contact</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <link rel="stylesheet" href="../styles.css" />
@@ -43,16 +43,16 @@ session_start();
             <div class="row">
                 <div class="col-md-6 offset-md-3">
                     <?php if (isset($_SESSION['message_success'])): ?>
-                    <div class="alert alert-success" role="alert">
-                        <?php echo htmlspecialchars($_SESSION['message_success']); ?>
-                    </div>
-                    <?php unset($_SESSION['message_success']); ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo htmlspecialchars($_SESSION['message_success']); ?>
+                        </div>
+                        <?php unset($_SESSION['message_success']); ?>
                     <?php endif; ?>
                     <?php if (isset($_SESSION['message_error'])): ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php echo htmlspecialchars($_SESSION['message_error']); ?>
-                    </div>
-                    <?php unset($_SESSION['message_error']); ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo htmlspecialchars($_SESSION['message_error']); ?>
+                        </div>
+                        <?php unset($_SESSION['message_error']); ?>
                     <?php endif; ?>
                     <form method="POST" action="../PHP/contact_processing.php">
                         <div class="mb-3">
@@ -93,7 +93,7 @@ session_start();
     <script src="../JS/script.js"></script>
     <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
